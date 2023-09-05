2130079c58a3da4daa2a11fd8de51d08a0d1230f
JaeHyeon Lee <112597963+fanta4715@users.noreply.github.com>
2023-09-05 10:59:54 +0900

    [Bronze I] Title: 팰린드롬수, Time: 120 ms, Memory: 13960 KB -BaekjoonHub

--- 백준/Bronze/1259. 팰린드롬수/팰린드롬수.java
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.java
@@ -1,13 +1,15 @@
 
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
         while (true){
-            String str =sc.nextLine();
+            String str =br.readLine();
             if (str.equals("0")) break;
 
             int len =str.length();
