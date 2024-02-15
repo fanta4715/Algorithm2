9219e89ada926eee58906b930526f615018904c4
JaeHyeon Lee <112597963+fanta4715@users.noreply.github.com>
2024-02-16 00:41:23 +0900

    [level 2] Title: 택배 배달과 수거하기, Time: 193.87 ms, Memory: 170 MB -BaekjoonHub

--- 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.java
+++ 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.java
@@ -1,33 +1,26 @@
 import java.util.*;
-//무조건 최대 개수만큼 싣고 가야 함
-//무조건 멀리 있는 곳을 가긴 해야 함
-//멀리있는 곳을 먼저가면 그 전 것들도 해결이 가능함. 
-//스택으로 배달 수거 스택 관리하자.
+
 class Solution {
     public long solution(int cap, int houseNum, int[] deliveries, int[] pickups) {
         Stack<Integer> deliveryStack = new Stack<>();
         Stack<Integer> pickupStack = new Stack<>();
         
-        //deliveries, pickup 배열을 순회하면서 스택에 넣음
-        for (int i = 0; i < deliveries.length; i++) {
-            for (int j=0; j< deliveries[i]; j++) {
-                deliveryStack.add(i+1);
+        setStack(deliveryStack, deliveries);
+        setStack(pickupStack, pickups);
+        
+        return getDistance(deliveryStack, pickupStack, cap);
+    }
+    private void setStack(Stack<Integer> stack, int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j=0; j< array[i]; j++) {
+                stack.add(i+1);
             }
         }
-        
-        for (int i = 0; i < pickups.length; i++) {
-            for (int j=0; j< pickups[i]; j++) {
-                pickupStack.add(i+1);
-            }
-        }
-        
-        //둘 다 비어있는 경우에 탈출
-        //그 외에는 루프 돌기
-        //1 3 3  delStack
-        //2 2 2  picStack
+    }
+    
+    private long getDistance(Stack<Integer> deliveryStack, Stack<Integer> pickupStack, int cap) {
         long answer = 0L;
         while (!deliveryStack.isEmpty() || !pickupStack.isEmpty()) {
-            //스택에서 하나 뺌 
             int destNum = 0;
             if (deliveryStack.isEmpty()) destNum = pickupStack.peek();
             else if (pickupStack.isEmpty()) destNum = deliveryStack.peek();
