419e5a260fad2027d3ee0374530603149ab098f0
JaeHyeon Lee <112597963+fanta4715@users.noreply.github.com>
2024-03-12 11:50:58 +0900

    [level 2] Title: 튜플, Time: 0.52 ms, Memory: 74 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.java
+++ 프로그래머스/2/64065. 튜플/튜플.java
@@ -2,32 +2,17 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String s) {
-        Set<Integer> set = new HashSet<>();
-        //1. 양 옆 중괄호 없애기
-        //{2},{2,1},{2,1,3},{2,1,3,4}
+        Set<String> set = new HashSet<>();
         String line = s.substring(1, s.length());
-        
-        //2. },{ split하기
-        //2 : 2,1 : 2,1,3 : 2,1,3,4
         String[] elements = line.replace("},{", ":").replace("{","").replace("}","").split(":");
-        
-        //3. 길이 순 정렬
-        Arrays.sort(elements, new Comparator<String>() {
-            @Override
-            public int compare(String str1, String str2) {
-                return str1.length() - str2.length();
-            }
-        });
+        Arrays.sort(elements, (a, b) -> {return a.length() - b.length();});
         int answer[] = new int[elements.length];
         int idx = 0;
-        for (int i=0; i<elements.length; i++) {
-            String[] nums = elements[i].split(",");
-            for (int j=0; j<nums.length; j++) {
-                int num = Integer.parseInt(nums[j]);
-                if (!set.contains(num)) {
-                    set.add(num);
-                    answer[idx++] = num;
-                    break;   
+        for (String s1 : elements) {
+            for (String s2 : s1.split(",")) {
+                if (set.add(s2)) {
+                    answer[idx++] = Integer.parseInt(s2);
+                    break;
                 }
             }
         }
