f3299b7ae7cdcd26cadbd7e68b3796241076f4e6
JaeHyeon Lee <112597963+fanta4715@users.noreply.github.com>
2024-03-15 12:33:48 +0900

    [level 2] Title: [3차] 압축, Time: 2.28 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.java
+++ 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.java
@@ -10,32 +10,23 @@ class Solution {
         }
         
         for (int i=0; i<msg.length(); i++) {
-            //find w
             String w= findW(map, msg, i);
-            
-            // list에 추가
             list.add(map.get(w));
             
-            //if 다음글자 존재 -> map.put
-            if (i+w.length() >= msg.length()) {
-                break;
-            }
+            if (i+w.length() >= msg.length()) break;
             
             map.put(msg.substring(i, i+w.length()+1), map.size()+1);
             i+=w.length()-1;
         }
+        
         int[] answer = list.stream().mapToInt(Integer::intValue).toArray();
+        
         return answer;
     }
     
     private String findW(Map<String, Integer> map, String msg, int idx) {
         for (int i=idx+1; i< msg.length()+1; i++) {
-            if (map.containsKey(msg.substring(idx, i))) {
-                continue;
-            }
-            else {
-                return msg.substring(idx, i-1);
-            }
+            if (!map.containsKey(msg.substring(idx, i))) return msg.substring(idx, i-1);
         }
         return msg.substring(idx, msg.length());
     }
