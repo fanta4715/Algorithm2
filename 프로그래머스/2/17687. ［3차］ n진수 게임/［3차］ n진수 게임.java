8b2d3ab7e7c9c8622bda86383d2eab787e189874
JaeHyeon Lee <112597963+fanta4715@users.noreply.github.com>
2024-03-15 12:58:25 +0900

    [level 2] Title: [3차] n진수 게임, Time: 1.84 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
+++ 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
@@ -1,14 +1,15 @@
 
 class Solution {
     public String solution(int n, int t, int m, int p) {
-        StringBuilder sb = new StringBuilder();
-        sb.append(" ");
-        for (int i=0; i<t*m; i++) {
-            sb.append(Integer.toString(i, n));
-            if (sb.length() > t*m) break;
+        StringBuilder sb = new StringBuilder(" ");
+        StringBuilder answer = new StringBuilder();
+        int num = 0;
+
+        while (sb.length() <= t*m) {
+            sb.append(Integer.toString(num++, n));
         }
         String str = sb.toString();
-        StringBuilder answer = new StringBuilder();
+        
         for (int i=0; i<t; i++) {
             answer.append(str.charAt(p+i*m));
         }
