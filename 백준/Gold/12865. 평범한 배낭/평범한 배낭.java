b9d20a60b149a425030cd39682cc79af6ae0fbec
JaeHyeon Lee <112597963+fanta4715@users.noreply.github.com>
2024-03-22 17:26:40 +0900

    [Gold V] Title: 평범한 배낭, Time: 184 ms, Memory: 53740 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -1,50 +1,36 @@
-import java.io.IOException;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
-public class Main {
+public class Main{
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st=new StringTokenizer(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        //N : 물품의 개수
-        //K : 가방의 최대 weight
-        int N = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
-
-        // w : 물품의 weight 배열
-        // v : 물품의 value 배열
-        int[] w = new int[N+1];
-        int[] v = new int[N+1];
-
-        //이 table에 DP 넣을 것
-        // K+1의 크기로 만드는 이유 : 1번 인덱스부터 쓰기 위함
-        int[] dp = new int[K+1];
-
-        //dp 배열 초기화
-        for (int i=0;i<K+1;i++){
-            dp[i]=0;
-        }
-
-        // w v의 안쓰는 0번 인덱스 초기화
-        w[0]=0; v[0]=0;
-
-        // 물품 N개의 w v 정보 채우기
-        for (int i=1;i<N+1;i++){
+        //n, k : 물품수, 무게
+        //weight : 무게
+        //value : 가치
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int[] weight = new int[n+1];
+        int[] value = new int[n+1];
+        for (int i=0; i<n; i++) {
             st = new StringTokenizer(br.readLine());
-            w[i]=Integer.parseInt(st.nextToken());
-            v[i]=Integer.parseInt(st.nextToken());
+            weight[i+1] = Integer.parseInt(st.nextToken());
+            value[i+1] = Integer.parseInt(st.nextToken());
         }
 
-
-        for (int i=1;i<N+1;i++){ //물품의 인덱스
-            for (int j=K;j>0;j--){ //dp의 인덱스 거꾸로
-                if (j-w[i]<0) continue;
-                else dp[j]=(dp[j]>dp[j-w[i]]+v[i])? dp[j]:dp[j-w[i]]+v[i];
+        //dp[물건수][weight]
+        int dp[][] = new int[n+1][k+1];
+        for (int i=1; i<n+1; i++) {
+            for (int j=1; j<k+1; j++) {
+                if (j < weight[i]) {
+                    dp[i][j] = dp[i-1][j];
+                    continue;
+                }
+                dp[i][j] = Math.max(dp[i-1][j], dp[i-1][j-weight[i]]+value[i]);
             }
         }
-        System.out.println(dp[K]);
+        System.out.println(dp[n][k]);
     }
 
 }
